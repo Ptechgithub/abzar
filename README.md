567b224367fe33511e7cae4383d8df3f6ad67740
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-03-26 16:12:54 +0330

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+
+🖐 [https://ptechgithub.github.io/abzar](https://ptechgithub.github.io/abzar)
+                
+
+### 1) برای تبدیل Reserved کانفیگ WireGuard از base 64 به decimal از لینک زیر استفاده کنید.
+برای استفاده و انتقال کانفیگ warp از Nekobox به V2ray استفاده میشود.
---
+++ script.js
@@ -0,0 +1,85 @@
+document.getElementById('base64Form').addEventListener('submit', event => {
+  event.preventDefault();
+  
+  const base64Input = document.getElementById('base64Input').value.trim();
+  
+  // Validate input length
+  if (base64Input.length !== 4) {
+    document.getElementById('result').innerText = `ورودی باید 4 کاراکتر باشد. شما فقط ${base64Input.length} کاراکتر وارد کرده‌اید.`;
+    return;
+  }
+  
+  // Validate input
+  if (!isValidBase64(base64Input)) {
+    document.getElementById('result').innerText = 'ورودی نامعتبر است! لطفا یک رشته Base64 معتبر با 4 کاراکتر وارد کنید.';
+    return;
+  }
+  
+  const hexString = Array.from(atob(base64Input), char => ('0' + char.charCodeAt(0).toString(16)).slice(-2)).join(',');
+  const decimalArray = hexString.split(',').map(hex => parseInt(hex, 16));
+
+  document.getElementById('result').innerHTML = `برای کپی کلیک کنید<br>${decimalArray.map(decimal => `<span class="copyable">${decimal}</span>`).join(', ')}`;
+  
+  // Add click event listener to copy the content of the clicked element
+  const copyableElements = document.querySelectorAll('.copyable');
+  copyableElements.forEach(element => {
+    element.addEventListener('click', () => {
+      const textToCopy = element.innerText;
+      navigator.clipboard.writeText(textToCopy)
+        .then(() => alert('مقدار کپی شد: ' + textToCopy))
+        .catch(err => console.error('خطا در کپی کردن مقدار:', err));
+    });
+  });
+});
+
+function isValidBase64(str) {
+  const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+  for (let i = 0; i < str.length; i++) {
+    if (validChars.indexOf(str.charAt(i)) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function handleLinkClick(event, pageName) {
+  event.preventDefault();
+  
+  var links = document.querySelectorAll('.menu a');
+  links.forEach(link => {
+    link.style.backgroundColor = '#007bff';
+    link.style.color = '#fff';
+  });
+  
+  event.target.style.backgroundColor = '#ffc107';
+  event.target.style.color = '#000';
+  
+  
+  if (pageName) {
+    window.location.href = pageName;
+  }
+}
+
+function displayMessage() {
+  var links = document.querySelectorAll('.menu a');
+  links.forEach(link => {
+    link.style.backgroundColor = '#007bff';
+    link.style.color = '#fff';
+  });
+  
+  event.target.style.backgroundColor = '#ffc107';
+  event.target.style.color = '#000';
+  alert('به زودی');
+}
+
+function openTelegramChannel() {
+  var links = document.querySelectorAll('.menu a');
+  links.forEach(link => {
+    link.style.backgroundColor = '#007bff';
+    link.style.color = '#fff';
+  });
+  
+  event.target.style.backgroundColor = '#ffc107';
+  event.target.style.color = '#000';
+  window.open('https://t.me/P_tech2024', '_blank');
+}
